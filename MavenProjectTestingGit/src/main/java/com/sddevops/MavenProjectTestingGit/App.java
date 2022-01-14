454076d48bcb1bd17d66e6700597ffa399ee763c
jaiwardhann82 <jaiwardhann82@gmail.com>
2022-01-14 09:56:42 -0800

    TEst

---
+++ MavenProjectTestingGit/src/main/java/com/sddevops/MavenProjectTestingGit/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.MavenProjectTestingGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
